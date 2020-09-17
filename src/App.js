39e3c2deffdb078ceea2531301f776186ae0741b
Amir9eng <amirmukhtarme@gmail.com>
2020-09-17 10:47:45 +0100

    feat: addtodo

--- src/App.js
+++ src/App.js
@@ -1,43 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 
 const id = (length = 3) => Math.random().toString(36).substring(2, 2 + length)
 
-const SearchBox = () => (
-    <div className="SearchBox">
-      <img src="/vectors/search-line.svg" alt="search-icon "></img>
-      <input type="text" placeholder="Search..."></input>  
-    </div>
-  
+const defaultNote = {}
+defaultNote[id()]=
+{
+  title: 'How to learn React in fivhhhhhhhhhhhhhe minutes',
+  content: 'Lorem ipsum dolor sit amet ',
+  dateCreated: Date.now(),
+  color: 'rgb(255,255,255)'
 
-)
-const TodoList = () => (
-  <div className="TodoList">
-  <p>How to learn React in five minutes</p>
-  <span>Sep 11,2020 at 10:30:47 AM</span>
-  </div>
-) 
-
-const TodoWrapper = () => (
-  <div className="TodoWrapper">
-    <h1>CV Notes</h1>
-    <SearchBox />
-    <TodoList />
-  </div>
-  
-  
-)
+}
 
 
 
 function App() {
+
+  const [note, setNote] = useState(defaultNote)
+
+
+  const NoteList = ({list}) => {
+    return (
+    Object.keys(list).map(noteid=> {
+      const note = list[noteid]
+      return(
+      <div key={noteid} className= "noteList">
+              <span >{note.title}</span>
+        <span >{note.dateCreated}</span>
+      </div>)
+    }))
+  }
+  
+  const SearchBox = () => (
+      <div className="SearchBox">
+        <img src="/vectors/search-line.svg" alt="search-icon "></img>
+        <input type="text" placeholder="Search..."></input>  
+      </div>
+    
+  
+  )
+  const TodoList = () => (
+    <div className="TodoList">
+    <p>How to learn React in five minutes</p>
+    <span>Sep 11,2020 at 10:30:47 AM</span>
+    </div>
+  ) 
+  
+  const TodoWrapper = () => (
+    <div className="TodoWrapper">
+      <h1>CV Notes</h1>
+      <SearchBox />
+      <NoteList list={note}/>
+
+    </div>
+    
+    
+  )
+  
+
+
+
   return (
     <div className="App">
-      
+
     <TodoWrapper />
     <div className="AddTodo">
       <div className="Todos">
+        <form>
         <input placeholder="Title"></input>
         <textarea placeholder="type your note..."></textarea>
         <div className="colorBoxes">
@@ -47,8 +78,9 @@ function App() {
         <img src="/vectors/Rectangle 8.svg"></img>
         <img src="/vectors/Rectangle 9.svg"></img>
         </div>
-        <span className="mark"><img src="/vectors/vector.svg"></img></span>
+        <span className= "mark"><img src="/vectors/vector.svg" /></span>
         </div>
+        </form>
       </div>
       <div>
       <h3>How to learn React in five minutes</h3>
